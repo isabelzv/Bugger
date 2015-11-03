74f84a455f0d8ca9f712e36ea3f9cac752c81a8f
isabelzv <isabelzv@Isabels-MacBook-Pro.local>
2015-11-03 07:27:13 -0700

    Got rid of remove and spawn functions (apparently they weren't doing anything. Completed handleInput function.

--- js/app.js
+++ js/app.js
@@ -17,11 +17,11 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    // TODO write loc and speed variables
-    this.x = -100; //start off screen???
-    // this.y = 101; 
+    // starts bugs off screen
+    this.x = -100;
+    // gives bugs a random row between 1 and 3
     this.y = getRandomInt(1, 4) * row;
-    // this.speed = random speed
+    // gives bugs a random speed
     this.speed = 75 + (getRandomInt(1, 10) * 10); 
 };
 
@@ -33,10 +33,12 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.y = this.y;
     this.x = this.x += this.speed * dt;
+    // if the enemies are off the screen x is reset to -100, a new random y value is given and a new random speed.
     if (this.x > canvas.width) {
         this.x = -100;
+        this.y = getRandomInt(1, 4) * row;
+        this.speed = 75 + (getRandomInt(1, 10) * 10);
     };
-    // this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -52,66 +54,58 @@ var Player = function() {
     this.x = col * 2;
     this.y = row * 4;
 };
-//TODO complete function
-Player.prototype.update = function() {
-    // this.x = this.x * dt;
-    // this.y = this.y * dt;
-    // if (this.x > canvas.width) {
-    //     this.x = canvas.width - canvas.col;
-    // };
-    // if (this.x < 0) {
-    //     this.x = 0;
-    // };
-    // if (this.y > canvas.height) {
-    //     this.y = canvas.height - canvas.row;
-    // };
-    // if (this.y < 0) {
-    //     this.y = 0;
-    // }; 
+
+Player.prototype.update = function(dt) {
+    this.x * dt;
+    this.y * dt;
 };
 
-// TODO complete function
 Player.prototype.render = function() {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
 };
 
-//TODO complete function
 Player.prototype.handleInput = function(key) {
-    
-};
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [];
-
-// Iterate through allEnemies. Remove offscreen enemies. If there are more than 3 enemies in the array push a new enemie. 
-function removeEnemies() {
-    var toRemove = [];
-    var i = 0;
-    //place all offscreen enemies in toRemove
-    allEnemies.forEach(function(enemy) {  
-        if (enemy.x > canvas.width) {
-            toRemove.push(enemy[i]);
-        }
-        i++;
-    });
-    // remove enemies in the toRemove array from allEnemies
-    toRemove.forEach(function(enemy) {
-        allEnemies.slice(enemy);
-    });
-};
-
-removeEnemies();
-
-// spawns new enemies if there is less than 3 enemies on the screen
-spawnEnemies = function() {
-    while (allEnemies.length < 3) {
-        allEnemies.push(new Enemy());
+    if (key === 'left' && this.x > 0) {
+        this.x -= 10;
+    } else if (key === 'right' && this.x < canvas.width - 100) {
+        this.x += 10;
+    } else if (key === 'up' && this.y > 0) {
+        this.y -= 10;
+    } else if (key === 'down' && this.y < canvas.height - 100) {
+        this.y += 10;
     };
 };
 
-spawnEnemies();
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+
+// Iterate through allEnemies. Remove offscreen enemies. If there are more than 3 enemies in the array push a new enemie. 
+// function removeEnemies() {
+//     var toRemove = [];
+//     var i = 0;
+//     //place all offscreen enemies in toRemove
+//     allEnemies.forEach(function(enemy) {  
+//         if (enemy.x > canvas.width) {
+//             toRemove.push(enemy[i]);
+//         }
+//         i++;
+//     });
+//     // remove enemies in the toRemove array from allEnemies
+//     toRemove.forEach(function(enemy) {
+//         allEnemies.slice(enemy);
+//     });
+// };
+
+// removeEnemies();
+
+// spawns new enemies if there is less than 3 enemies on the screen
+// spawnEnemies = function() {
+//     while (allEnemies.length < 3) {
+//         allEnemies.push(new Enemy());
+//     };
+// };
+
+// spawnEnemies();
 
 // Place the player object in a variable called player
 var player = new Player();
